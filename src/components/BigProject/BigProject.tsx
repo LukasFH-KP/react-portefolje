88d47627989ae66bcd6f2f459b6aebeaa41cf01e
julerissen <lukasfholmen@gmail.com>
2025-05-30 13:23:14 +0200

    Added dot to public paths

--- src/components/BigProject/BigProject.tsx
+++ src/components/BigProject/BigProject.tsx
@@ -15,7 +15,7 @@ const BigProject = ({ name, description, long_description, thumbnail_path, exter
             <hr/>
 
             <div className="ContentBox">
-                <img className="Thumbnail" src={"/" + thumbnail_path} alt=""/>
+                <img className="Thumbnail" src={"./" + thumbnail_path} alt=""/>
                 <div className="VR"/>
 
                 <div className="DescriptionBox">
--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -15,7 +15,7 @@ export type ProjectProps = {
 const Project = ({ name, description, thumbnail_path, id, animation }: ProjectProps)=> {
     return (
         <motion.div className="Project" style={animation[0]} animate={animation[1]}>
-            <img src={"/" + thumbnail_path} alt=""/>
+            <img src={"./" + thumbnail_path} alt=""/>
             <div className="VR"></div>
             <div className="ContentBox">
                 <h2>{name}</h2>
